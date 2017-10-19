a501211c44a9173520998baf5b031ce661d54f6f
hm-hofmann4 <hm-hofmann4@HM07000-L-RA-01.ads.mwn.de>
2017-10-19 11:28:45 +0200

    test entfernt

--- src/main/java/edu/hm/dako/chat/client/ClientFxGUI.java
+++ src/main/java/edu/hm/dako/chat/client/ClientFxGUI.java
@@ -30,7 +30,7 @@ public class ClientFxGUI extends Application implements ClientUserInterface {
 
 	private static Log log = LogFactory.getLog(ClientFxGUI.class);
 	
-	//dsddsd
+	
 
 	private Stage stage;
 	private static LogInGuiController lc;
